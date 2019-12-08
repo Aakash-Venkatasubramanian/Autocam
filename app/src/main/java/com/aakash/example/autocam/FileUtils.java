a58d197370aaa4aa58823b6a96bde9e910196b83
Aakash Venkatasubramanian <aakashvenkatasubramanian@gmail.com>
2019-12-08 08:30:05 +0530

    Upgrade, check README

---
+++ app/src/main/java/com/aakash/example/autocam/FileUtils.java
@@ -0,0 +1,33 @@
+package com.aakash.example.autocam;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class FileUtils {
+    private FileUtils() {
+    }
+
+    public static File saveImage(byte[] content, String extension) throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = new File(storageDir, timeStamp + "." + extension);
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(image);
+            out.write(content);
+            out.close();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+        return image;
+    }
+}
--- app/src/main/java/com/aakash/example/autocam/ui/camera/CameraSourcePreview.java
+++ app/src/main/java/com/aakash/example/autocam/ui/camera/CameraSourcePreview.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.aakash.example.autocam.ui.camera;
 
 import android.content.Context;
@@ -30,13 +15,11 @@ import java.io.IOException;
 
 public class CameraSourcePreview extends ViewGroup {
     private static final String TAG = "CameraSourcePreview";
-
     private Context mContext;
     private SurfaceView mSurfaceView;
     private boolean mStartRequested;
     private boolean mSurfaceAvailable;
     private CameraSource mCameraSource;
-
     private GraphicOverlay mOverlay;
 
     public CameraSourcePreview(Context context, AttributeSet attrs) {
@@ -54,9 +37,7 @@ public class CameraSourcePreview extends ViewGroup {
         if (cameraSource == null) {
             stop();
         }
-
         mCameraSource = cameraSource;
-
         if (mCameraSource != null) {
             mStartRequested = true;
             startIfReady();
@@ -89,8 +70,6 @@ public class CameraSourcePreview extends ViewGroup {
                 int min = Math.min(size.getWidth(), size.getHeight());
                 int max = Math.max(size.getWidth(), size.getHeight());
                 if (isPortraitMode()) {
-                    // Swap width and height sizes when in portrait, since it will be rotated by
-                    // 90 degrees
                     mOverlay.setCameraInfo(min, max, mCameraSource.getCameraFacing());
                 } else {
                     mOverlay.setCameraInfo(max, min, mCameraSource.getCameraFacing());
@@ -133,31 +112,22 @@ public class CameraSourcePreview extends ViewGroup {
                 height = size.getHeight();
             }
         }
-
-        // Swap width and height sizes when in portrait, since it will be rotated 90 degrees
         if (isPortraitMode()) {
             int tmp = width;
             width = height;
             height = tmp;
         }
-
         final int layoutWidth = right - left;
         final int layoutHeight = bottom - top;
-
-        // Computes height and width for potentially doing fit width.
         int childWidth = layoutWidth;
         int childHeight = (int)(((float) layoutWidth / (float) width) * height);
-
-        // If height is too tall using fit width, does fit height instead.
         if (childHeight > layoutHeight) {
             childHeight = layoutHeight;
             childWidth = (int)(((float) layoutHeight / (float) height) * width);
         }
-
         for (int i = 0; i < getChildCount(); ++i) {
             getChildAt(i).layout(0, 0, childWidth, childHeight);
         }
-
         try {
             startIfReady();
         } catch (IOException e) {
@@ -173,7 +143,6 @@ public class CameraSourcePreview extends ViewGroup {
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             return true;
         }
-
         Log.d(TAG, "isPortraitMode returning false by default");
         return false;
     }
